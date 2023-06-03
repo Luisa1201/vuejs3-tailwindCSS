eb3f9c03a2bedf5a49936cf746204759e36d8bdc
Luisa Fernanda Acosta Vergel <lfacostav@ufpso.edu.co>
2023-06-03 12:23:02 -0500

    Ejercicio de vuejs3 con TailwindCSS

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+ import TaskComponent from '@/components/TaskComponent.vue';
+ import {ref} from 'vue';
+
+ let newTask
+
+ const tasksList = ref([
+  'Levantarse',
+  'Cruzar el semáforo sin mirar',
+  'Sálvase quien pueda'
+ ])
+
+ const addTask = () => {
+  tasksList.value.unshift(newTask)
+ }
+
+ const deleteFromArray = (index) => {
+  tasksList.value.splice(index, 1)
+  console.log('Kill', tasksList)
+ }
+
+</script>
+
+<template>
+  <div class="px-4 py-3 mx-auto mt-12 space-y-2 overflow-hidden rounded shadow-md bg-gray-50 w-96">
+    <h1 class="text-2xl font-semibold">Lista de Tareas</h1>
+
+    <div class="flex">
+      <input type="text"  class="w-4/5 h-8 px-2 py-1 border border-r-0 border-green-600 rounded rounded-r-none" v-model="newTask">
+      <span class="w-1/5 h-8 text-xl font-bold text-center text-whie bg-green-600 border-l-0 rounded rounded-l-none leading-0 hover:bg-green-700 hover:cursor-pointer" @click="addTask">
+        +
+      </span>
+
+
+    </div>
+
+ <TaskComponent v-for="(task, index) in tasksList"
+ :key="index" :name="task" 
+ :index="index" 
+ @emitDeleteTask="deleteFromArray"/>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: 'No tengo nombre'
+    },
+    index: {
+        type: Number
+    }
+})
+
+const emit = defineEmits([
+    'emitDeleteTask'
+])
+
+const deleteTask = (index) => {
+    console.log('Borrar', index)
+    emit('emitDeleteTask', index)
+
+}
+
+</script>
+
+<template>
+    <div class="flex justify-between px-4 py-2 bg-gray-200 rounded">
+        {{ props.name }}
+
+        <span class="inline-block px-2 text-white bg-red-400 rounded hover:cursor-pointer hover:bg-red-600" @click="deleteTask(props.index)">
+            X
+        </span>
+    </div>
+    
+</template>
